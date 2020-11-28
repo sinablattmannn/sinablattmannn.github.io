0f748322f6dcee2dbd4141951b6bd4fbb523607f
sinablattmannn <69575147+sinablattmannn@users.noreply.github.com>
2020-11-28 16:34:42 +0100

    change

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const calendarContainer = document.querySelector(".container");
 var currentDate = new Date();
 
 const calendarDays = 24;
-console.log(currentDate.getDate());
+console.log(currentDate.getDate() >= event.target.innerHTML);
+console.log(currentDate.getMonth() + 1 === 12);
 const openDoor = (path, event) => {
   if (currentDate.getMonth() + 1 === 12 && currentDate.getDate() >= event.target.innerHTML) {
     event.target.parentNode.style.backgroundImage = `url(${path})`;
